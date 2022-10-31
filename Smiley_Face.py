8fdc1e9d68c91a24b14455f5692b5aced423cc7e
Trevor Erickson <erickson.trevor.g@gmail.com>
2022-10-30 22:38:17 -0400

    Added mouth

--- Smiley_Face.py
+++ Smiley_Face.py
@@ -1,26 +1,26 @@
 import turtle
 
-def face_base(a,b):
+def face_base(c,r):
     turtle.penup()
-    turtle.forward(b)
+    turtle.forward(r)
     turtle.left(90)
     turtle.pendown()
-    turtle.fillcolor(a)
+    turtle.fillcolor(c)
     turtle.begin_fill()
-    turtle.circle(b)
+    turtle.circle(r)
     turtle.end_fill()
     turtle.penup()
     turtle.home()
     turtle.pendown()
 
-def nose(a,b):
+def nose(c,r):
     turtle.penup()
-    turtle.forward(b)
+    turtle.forward(r)
     turtle.left(90)
     turtle.pendown()
-    turtle.fillcolor(a)
+    turtle.fillcolor(c)
     turtle.begin_fill()
-    turtle.circle(b)
+    turtle.circle(r)
     turtle.end_fill()
     turtle.penup()
     turtle.home()
@@ -58,8 +58,22 @@ def eye(r,c,x,y):
     turtle.home()
     turtle.pendown()
 
+def mouth(r):
+    turtle.penup()
+    turtle.goto(-r*.6,-r*.25)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.fillcolor("black")
+    turtle.begin_fill()
+    turtle.circle(r*.6,180)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+
 
 def main():
+    turtle.speed(6)
     face_color = "gold"
     face_size = 100
     face_base(face_color,face_size)
@@ -70,6 +84,8 @@ def main():
     iris_color = "dodgerblue3"
     eye(face_size/4,iris_color,face_size*.35,face_size*.25)
     eye(face_size/4,iris_color,face_size*-.35,face_size*.25)
+
+    mouth(face_size)
 
     input("Awaiting imput: ")
 
