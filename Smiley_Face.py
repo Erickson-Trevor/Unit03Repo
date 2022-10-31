c7b220d7f48846c7c3d00d1bf8819cb3a7f7862d
Trevor Erickson <erickson.trevor.g@gmail.com>
2022-10-30 22:03:17 -0400

    Centered face and added nose

--- Smiley_Face.py
+++ Smiley_Face.py
@@ -1,15 +1,38 @@
 import turtle
 
-def face(a,b):
+def face_base(a,b):
+    turtle.penup()
+    turtle.forward(b)
+    turtle.left(90)
+    turtle.pendown()
     turtle.fillcolor(a)
     turtle.begin_fill()
     turtle.circle(b)
     turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+
+def nose(a,b):
+    turtle.penup()
+    turtle.forward(b)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.fillcolor(a)
+    turtle.begin_fill()
+    turtle.circle(b)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+
 
 def main():
     face_color = "gold"
     face_size = 100
-    face(face_color,face_size)
+    face_base(face_color,face_size)
+    nose_color = "indianred1"
+    nose(nose_color, face_size*.1)
     input("Awaiting imput: ")
 
 main()
