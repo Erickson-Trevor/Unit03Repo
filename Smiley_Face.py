c7fbc58075d7857c1895050210c7924934eaa647
Trevor Erickson <erickson.trevor.g@gmail.com>
2022-10-30 22:25:57 -0400

    Added eyes

--- Smiley_Face.py
+++ Smiley_Face.py
@@ -26,13 +26,51 @@ def nose(a,b):
     turtle.home()
     turtle.pendown()
 
+def eye(r,c,x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.forward(r)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.fillcolor("white")
+    turtle.begin_fill()
+    turtle.circle(r)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(r/2)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.fillcolor(c)
+    turtle.begin_fill()
+    turtle.circle(r/2)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.left(90)
+    turtle.forward(r/4)
+    turtle.right(90)
+    turtle.pendown()
+    turtle.fillcolor("black")
+    turtle.begin_fill()
+    turtle.circle(r/4)
+    turtle.end_fill()
+    turtle.penup()
+    turtle.home()
+    turtle.pendown()
+
 
 def main():
     face_color = "gold"
     face_size = 100
     face_base(face_color,face_size)
+
     nose_color = "indianred1"
     nose(nose_color, face_size*.1)
+
+    iris_color = "dodgerblue3"
+    eye(face_size/4,iris_color,face_size*.35,face_size*.25)
+    eye(face_size/4,iris_color,face_size*-.35,face_size*.25)
+
     input("Awaiting imput: ")
 
 main()
