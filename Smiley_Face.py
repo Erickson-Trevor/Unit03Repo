1f2dfcd7afc7bc794703aa15031bb07abe823690
Trevor Erickson <erickson.trevor.g@gmail.com>
2022-11-02 15:02:15 -0400

    Added a randomized iris color and position for every face iteration. Added a promt that requests # of faces.

--- Smiley_Face.py
+++ Smiley_Face.py
@@ -1,4 +1,6 @@
 import turtle
+import random
+
 
 def face_base(c,r,x,y):
     turtle.penup()
@@ -59,6 +61,23 @@ def eye(r,c,x,y,x1,y1):
     turtle.goto(x,y)
     turtle.pendown()
 
+def get_hex():
+    x = random.randint(0,15)
+    if x == 0:
+        return "0"
+    else:
+        return hex(x).lstrip("0x")
+
+def iris_color():
+    iris_color_red_a = get_hex()
+    iris_color_red_b = get_hex()
+    iris_color_green_a = get_hex()
+    iris_color_green_b = get_hex()
+    iris_color_blue_a = get_hex()
+    iris_color_blue_b = get_hex()
+    iris_color = "".join(["#",iris_color_red_a,iris_color_red_b,iris_color_green_a,iris_color_green_b,iris_color_blue_a,iris_color_blue_b])
+    return iris_color
+
 def mouth(r,x,y):
     turtle.penup()
     turtle.goto(-r*.6+x,-r*.25+y)
@@ -73,7 +92,7 @@ def mouth(r,x,y):
     turtle.pendown()
 
 
-def main(x,y,c):
+def draw_face(x,y,c):
     turtle.speed(12)
     face_color = "gold"
     face_size = 100
@@ -82,14 +101,17 @@ def main(x,y,c):
     nose_color = "indianred1"
     nose(nose_color, face_size*.1,x,y)
 
-    iris_color = c
-    eye(face_size/4,iris_color,x,y,face_size*.35+x,face_size*.25+y)
-    eye(face_size/4,iris_color,x,y,face_size*-.35+x,face_size*.25+y)
+    eye(face_size/4,c,x,y,face_size*.35+x,face_size*.25+y)
+    eye(face_size/4,c,x,y,face_size*-.35+x,face_size*.25+y)
 
     mouth(face_size,x,y)
 
+def main():
+    j = input("# of faces: ")
+    j = int(j)
+    i = 0
+    while i < j:
+        draw_face(random.randint(-250,250),random.randint(-250,250),iris_color())
+        i = i+1
 
-main(0,0,"firebrick")
-main(200,-169,"dimgray")
-main(300,30,"dodgerblue3")
-input("Awaiting Input: ")
+main()
